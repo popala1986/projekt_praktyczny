70178fe39a2ffa613cca695e212ebbb5275b020c
popala1986 <p.opala1986@gmail.com>
2023-05-13 10:46:28 +0200

    First Commit

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -4,6 +4,9 @@ package com.sda;
 
 public class Main {
     public static void main(String[] args) {
+        if (true){
+            System.out.printf("blabla");
+        }
         System.out.println("test");
     }
 }
